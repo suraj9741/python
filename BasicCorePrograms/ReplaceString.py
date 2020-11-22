abbc5e1a64d879dc112abcd687b56aa2553bf699
suraj9741 <surajjadhav9741@gmail.com>
2020-11-22 23:21:36 +0530

    Adding Replace String Template program

--- BasicCorePrograms/ReplaceString.py
+++ BasicCorePrograms/ReplaceString.py
@@ -1,14 +1,24 @@
-def main():
-    UserName = input("Enetr the User Name : ")
-    Lenght = len(UserName)
-    if Lenght > 3 :
-        print (f'Hello {UserName}, How are you?')
-    else :
-        print('Enter min 3 char')
-    x=1+2j
-    print(type(x))
-    a = {5, 2, 3, 1, 4}
-    print("a = ", a)
+"""
+   * author - ${Suraj Jadhav}
+   * date - ${22-Nov-20}
+   * time - ${10:55 PM}
+   * package - ${PACKAGE_NAME}
+   * Title - User Input and Replace String Template “Hello <<UserName>>, How are you?”
+"""
+class ReplaceString :
+    def __init__(self):                                 # constructor
+        self.UserName = " "
+    def main(self):                                     # Method To Accepting and check the min character condition
+        self.UserName = input("Enetr the User Name : ")
+        Lenght = len(self.UserName)
+        if Lenght > 3 :
+            print ('Hello '+ self.UserName +', How are you?')
+        else :
+            print('Enter min 3 char')
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__':                              # Main method
+    try :                                               # Exception Handling
+        object = ReplaceString()
+        object.main()
+    except :
+        print('Exception Raised.')
